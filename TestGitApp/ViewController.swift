05fe9b92607681122ab70e517ff822012bc3405a
Admin <>
2019-07-14 18:45:35 +0430

    this is first message for commit

---
+++ TestGitApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TestGitApp
+//
+//  Created by Admin on 7/14/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("hi jack")
+    }
+
+
+}
+
